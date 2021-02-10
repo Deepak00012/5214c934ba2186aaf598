18b5ccb8132958e9c39491aa674790ec7be2f362
Deepak00012 <78842064+Deepak00012@users.noreply.github.com>
2021-02-10 17:07:00 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,10 @@ import React from 'react';
 class App extends React.Component {
   constructor(props){
     super(props);
-    this.state= {value:''};
+    this.state= {value:'',
+  isClicked: false
+  };
+    
   }
   change = (event)=>{
     this.setState({value: event.target.value});
@@ -15,19 +18,41 @@ class App extends React.Component {
     event.preventDefault();
 
   }
+  click = ()=>{
+    this.setState({
+    isClicked: true
+    })
+  }
+  componentDidMount(){
+    const url = 'https://restcountries.eu/rest/v2/name/{{FORM_INPUT_HERE}}';
+    fetch(url)
+    .then((response) => response.json())
+    .then((data)=>console.log(data));
+    
+   
+  }
+ 
 
   render(){
+    let clicked="";
+    if(this.state.value){
+      clicked= <button>Capital Weather</button>;
+    }else{
+      clicked="";
+    }
   return (
     <div className="App">
       <form onSubmit={this.submit}>
         <label>Country
           <input type="text" value={this.state.value} placeholder="Enter country" onChange={this.change} />
           </label>
-          <input disabled={this.state.value.length<1} type="submit" value="Submit" />
+          <input disabled={this.state.value.length<1} type="submit" value="Submit" onClick={this.click} onClick={this.componentDidMount} />
+          {clicked}
         </form>
     </div>
   );
 }
 }
+
 
 export default App;
